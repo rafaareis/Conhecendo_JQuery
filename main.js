505f614e83fe7aeb206a8cbc3d7be786a3be9643
Rafael de Alvarenga Reis <rafaareis.ebac@gmail.com>
2023-04-05 14:25:22 -0300

    Acompanhamento da Aula 02

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
 $(document).ready(function() {
-    console.log(document.querySelector('header button'));
-    console.log($('header button'));
-    console.log($('#botao-cancelar'));
+    // JavaScript: console.log(document.querySelector('header button'));
+    // JQuery: console.log($('header button'));
+    
+    //[JavaScript] 
+    //document.querySelector('header button').addEventListener('click', function() {})
+
+    //[JQuery]
+    $('header button').click(function () {
+        alert(`Expandir formulário`)
+    })
+    
+    $('form').on('submit', function(e) {
+        e.preventDefault();
+        console.log('fui clicado');
+    })
+
 })
