02e999458b62c42ad4f4cd8887d80004712f2b8f
Rafael de Alvarenga Reis <rafaareis.ebac@gmail.com>
2023-04-05 21:59:06 -0300

    Commit 05042023-2158: Acompanhamento da Aula 4

--- main.js
+++ main.js
@@ -17,7 +17,26 @@ $(document).ready(function() {
     
     $('form').on('submit', function(e) {
         e.preventDefault();
-        console.log('fui clicado');
+        const enderecoDaNovaImagem = $('#endereco-imagem-nova').val();
+        console.log(enderecoDaNovaImagem);
+
+        const novoItem = $(`<li style="display: none"></li>`);
+        $(`<img src="${enderecoDaNovaImagem}"  />`).appendTo(novoItem);
+        $(`
+        <div class="overlay-imagem-link">
+        <a href="${enderecoDaNovaImagem}" title="Ver imagem em tamanho real" target="_blank">
+            Ver imagem em tamanho real
+        </a>
+        </div>
+
+        `).appendTo(novoItem);
+        $(novoItem).appendTo('ul');
+        $(novoItem).fadeIn(1000);
+        $('#endereco-imagem-nova').val('')
+
+        //console.log('fui clicado');
     })
 
 })
+
+// Parou no no minuto 08:31. Continuar depois.
